19f0ca6a1e3ff2f2a5763746bf654884840fdb0f
LeopoldoGitHub <leopoldofloresc@gmail.com>
2024-04-22 07:29:33 -0300

    Actualización de streamlit

--- streamlit/pages/3_Visualización.py
+++ streamlit/pages/3_Visualización.py
@@ -12,9 +12,9 @@ st.title("Análisis de datos")
 st.write("## Análisis de datos:")
 st.write("A través de Power BI realizamos un análisis de los datos del precio de combustible, que se pueden consultar de manera interactiva en el siguiente dashboard:")
 
-# with st.expander('Dashboard'):
-img = Image.open("../s14-24-m-data-bi\Media\PowerBI_Inicio.jpg")
-imagen = img.resize((800, 400))
-st.image(imagen)    
+with st.expander('Dashboard'):
+    # img = Image.open("./s14-24-m-data-bi/Media/PowerBI_Inicio.jpg")
+    # imagen = img.resize((800, 400))
+    # st.image(imagen)    
 
-    # st.write("<iframe width='1000' height='600' src='https://app.powerbi.com/view?r=eyJrIjoiYmMxMWIwMWYtNDFjMy00NDIyLTg3ODQtMzM5MTJiZDk3M2VhIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9' style='display:block;margin:auto;'></iframe>", unsafe_allow_html=True)
+    st.write("<iframe width='1000' height='600' src='https://app.powerbi.com/view?r=eyJrIjoiYmU2ZDBiMTQtMzQ2Yy00Yjg3LWJhN2MtMmE1OTdiYjcxMzMyIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9 ' style='display:block;margin:auto;'></iframe>", unsafe_allow_html=True)
